68dd2064bfc40c77e6e3ba394fda3f05ac96ab02
Kajal817 <87418349+Kajal817@users.noreply.github.com>
2025-04-20 20:38:18 +0530

    push

--- Registry/src/main/java/com/art/RegistryApplication.java
+++ Registry/src/main/java/com/art/RegistryApplication.java
@@ -11,6 +11,7 @@ public class RegistryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryApplication.class, args);
 		System.out.println("Hello to Github added in Node branch");
+		System.out.println("Hello to Github added in Node branch");
 	}
 
 }
